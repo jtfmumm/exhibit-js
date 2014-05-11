5b0ba01665201eab5330c9b739fc2836093af611
John Mumm <jtfmumm@gmail.com>
2014-05-10 20:38:46 -0400

    Initial configuration. App loading

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+require.config({
+    baseUrl: "/js",
+    paths: {
+        jquery: "lib/jquery-2.1.1", 
+        underscore: "lib/underscore", 
+        backbone: "lib/backbone"
+    }
+});
+
+require(["jquery", "backbone", "underscore"], function ($, Backbone, _) {
+    console.log("hi");
+    // Dependencies are loaded...
+    // Execute code
+});
